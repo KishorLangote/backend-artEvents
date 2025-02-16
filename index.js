7388488ddab5c9f75941f9be13571e3237574c27
Kishor Langote <kishorlangoet2@gmail.com>
2025-02-17 04:56:05 +0530

    feat: Adding apis to get the art events details also event get by title and tags.

---
+++ index.js
@@ -0,0 +1,155 @@
+const express = require("express")
+require("dotenv").config()
+const app = express()
+
+const cors = require("cors") // handle cross origin request..
+const corsOptions = {
+  origin: "*",              // this allows request from any origin
+  credentials: true,        // this allows cookies, authorization headers, TLS certi..
+  optionSuccessStatus: 200, // set status code 200 fir successful request..
+}
+
+app.use(cors(corsOptions))  // this is cors middleware, allowing the browser to make cross origin request to the server..
+
+
+const { initializeDatabase} = require("./db/db.connect")
+const artEvents = require("./models/artEvents.models")
+
+app.use(express.json()) // middleware..
+
+initializeDatabase() // call the function..
+
+app.get("/", (req, res) => {
+  res.send("Hello, express server!!")
+})
+
+
+async function createArtEvent(newArtEvent){
+  try{
+    const events = new artEvents(newArtEvent) // create new event
+    const savedEvents = await events.save()  // save event
+    return savedEvents;
+  } catch(error) {
+    throw error;
+  }
+}
+
+
+// to create new art events entry in db via postman using POST method: 
+
+app.post("/artEvents", async (req, res) => {
+  try{
+    const saveArtEvent = await createArtEvent(req.body)
+    res.status(201).json({ message: "Art event added successfully.", artEvent: saveArtEvent})
+  } catch(error) {
+    res.status(500).json({ error: "Failed to add art event."})
+  }
+})
+
+
+// to get all art event database: 
+
+async function readAllArtEvents(){
+  try{
+    const allArtEvents = artEvents.find()
+    return allArtEvents;
+  }catch(error){
+    throw error;
+  }
+}
+
+app.get("/artEvents", async (req, res) => {
+  try{
+    const artEvents = await readAllArtEvents()
+    if(artEvents.length > 0){
+      res.json(artEvents)
+    } else {
+      res.status(404).json({ error: "No art event found."})
+    }
+  }catch(error){
+    res.status(500).json({ error: "Failed to fetch art events data."})
+  }
+})
+
+// art event read by id:
+
+async function readArtEventById(artEventId){
+  try{
+    const artEventById = await artEvents.findById(artEventId)
+    return artEventById
+  }catch(error){
+    throw error;
+  }
+}
+
+
+app.get("/artEvents/:artEventId", async (req, res) => {
+  try{
+    const artEvents = await readArtEventById(req.params.artEventId)
+    if(artEvents){
+      res.json(artEvents)
+    } else {
+      res.status(404).json({ error: "Art event not found."})
+    }
+  }catch(error){
+    res.status(500).json({ error: "Failed to fetch art event data."})
+  }
+})
+
+
+//  APIs route for read art event by title: 
+
+async function readArtEventByTitle(artEventTitle){
+  try{
+    const artEventByTitle = await artEvents.find({ title: artEventTitle })
+    return artEventByTitle;
+  }catch(error){
+    throw error;
+  }
+}
+
+app.get("/artEvents/title/:artEventTitle", async (req, res) => {
+  try{
+    const artEvents = await readArtEventByTitle(req.params.artEventTitle)
+    if(artEvents){
+      res.json(artEvents)
+    } else {
+      res.status(404).json({ error: "Not found art event."})
+    }
+  }catch(error){
+    res.status(500).json({ error: "Failed to fetch art event data."})
+  }
+})
+
+
+// APIs route to read art event by tags..
+
+async function readArtEventByTags(artEventTag){
+  try{
+    const artEventByTag = await artEvents.findOne({tags: artEventTag })
+    return artEventByTag;
+  }catch(error){
+    throw error;
+  }
+}
+
+app.get("/artEvents/tag/:artEventTag", async (req, res) => {
+  try {
+    const artEvents = await readArtEventByTags(req.params.artEventTag)
+    if(artEvents){
+      res.json(artEvents)
+    } else {
+      res.status(404).json({ error: "No art event found."})
+    }
+  } catch(error){
+    res.status(500),json({ error: "Failed to fetch art event data."})
+  }
+})
+
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server is running on the port ${PORT}`)
+})
---
+++ models/artEvents.models.js
@@ -0,0 +1,209 @@
+// const mongoose = require("mongoose")
+
+// const artEventSchema = new mongoose.Schema({
+//     title: {
+//       type: String,
+//     },
+
+//     date: {
+//       type: String,
+//     },
+
+//     continent: {
+//       type: String,
+//     },
+
+//     eventImageUrl: {
+//       type: String,
+//     },
+
+//     host: [{
+//       type: String,
+//       enum: ["Artist", "Writer", "Poet" ]
+//     }],
+
+//     hostImageUrl: {
+//       type: String,
+//     },
+
+//     descriptions: {
+//       type: String,
+//     },
+
+//     artGalleryName: {
+//       type: String,
+//     },
+
+//     startEvent: {
+//       type: String,
+//     },
+
+//     endEvent: {
+//       type: String,
+//     },
+
+//     address: {
+//       type: String,
+//     },
+
+//     googleMapImageUrl: {
+//       type: String,
+//     },
+
+//     entryFee: {
+//       type: String,
+//     },
+
+//     artistName: {
+//       type: String,
+//     },
+
+//     artistImageUrl: [{
+//       type: String,
+//     }],
+
+//     officialWebsite: {
+//       type: String,
+//     },
+
+//     artistCount: {
+//       type: Number,
+//     },
+// },
+// {
+//   timestamps: true,
+// }
+// )
+
+// const ArtEvents = mongoose.model("ArtEvent", artEventSchema)
+
+// module.exports = ArtEvents;
+
+const mongoose = require("mongoose");
+
+const artEventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+
+    date: {
+      type: String,
+      required: true
+     },
+
+    eventType: {
+      type: String,
+      enum: ["Exhibition", "Workshop", "Live Performance", "Cultural Exhibition", "Permanent Exhibition", "Sculpture & Digital Art", "Multimedia Exhibition", "Historical Exhibition", "Contemporary Exhibition", "Permanent Collection"],
+    },
+
+    continent: { 
+      type: String 
+    },
+
+    country: {
+       type: String
+      },
+
+    city: { 
+      type: String 
+    },
+
+    eventImageUrl: { 
+      type: String 
+    },
+
+    host: [{ 
+      type: String,
+      enum: ["Artist", "Writer", "Poet", "Curator","Art Historian", "Museum Director", "Art Restorer", "Gallery Owner", "Art Collector", "Senior Curator", "Art Critic", "Archaeologist", "Cultural Historian", "Art Conservator", "Guest Artist", "Cultural Expert", "Historian", "Renowned Sculptor", "Digital Artist", "Art Therapist", "Chief Curator", "Architect", "Modern Art Expert", "American Art Curator", "Museum Educator", "Art Curator"] 
+      }],
+
+    hostImageUrl: {
+       type: String 
+      },
+
+    descriptions: { 
+      type: String
+     },
+
+    artGalleryName: {
+       type: String 
+      },
+
+    startEvent: {
+       type: String
+       },
+
+    endEvent: { 
+      type: String
+     },
+
+    address: { 
+      type: String 
+    },
+
+    coordinates: {
+      latitude: { type: Number },
+      longitude: { type: Number }
+    },
+
+    googleMapImageUrl: { 
+      type: String
+     },
+
+    entryFee: { 
+      type: String 
+    },
+
+    artistCount: {
+       type: Number 
+      },
+
+    maxCapacity: { 
+      type: Number
+     },
+
+    ticketsAvailable: { 
+      type: Boolean,
+       default: true 
+      },
+
+    contactInfo: {
+      email: { type: String },
+      phone: { type: String },
+    },
+
+    sponsors: [{ 
+      type: String 
+    }],
+
+    featuredArtists: [
+      {
+        name: { type: String },
+        imageUrl: { type: String },
+        artWorkImageUrl: [
+          {
+            imageUrl:{ type: String },
+            title: { type: String }
+          } 
+        ],
+      },
+    ],
+      
+
+    tags: [{ 
+      type: String,
+      enum: ["Modern Art", "Painting", "Sculpture", "Ceramics", "Photography", "Realism", "Impressionism", "Surrealism", "Cubism", "Abstract Expressionism", "Pop Art", "Classic Art", "Renaissance", "Middle Eastern Art", "Contemporary", "Cultural Fusion", "Latin Art", "Modernism", "Modern Indian Art", "Biennale", "Kala Ghoda", "Deccan Art", "Historical Artifacts", "Textile Arts", "Colonial Art", "Indian Renaissance", "Indian Art", "Culture", "History", "Goan Art", "Post-Impressionism", "Dutch Masters", "Digital Art", "French Art", "20th Century Art", "New York", "Modern Architecture", "American Gothic", "Real Illusion", "Abstract", "Cubism", "Regionalism", "Sketch", "Cartoon", "Contemporary Art"]
+    }],
+
+    officialWebsite: { 
+      type: String 
+    },
+  },
+  { timestamps: true }
+);
+
+const ArtEvents = mongoose.model("ArtEvent", artEventSchema);
+
+module.exports = ArtEvents;
